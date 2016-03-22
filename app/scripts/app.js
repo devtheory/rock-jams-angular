d8ec215f65afe53ba876b2890709c180e9a3cf9b
Jose Sanchez <jparkers0012@gmail.com>
2016-03-22 12:15:12 -0400

    3 routing-states cp

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,27 @@
-angular.module('rockJams', []);
+(function(){
+  function config($stateProvider, $locationProvider){
+    $locationProvider
+      .html5Mode({
+        enabled: true, //for no hashbang urls
+        requireBase: false //helps prevent location errors
+      });
+
+    $stateProvider
+      .state('landing', {
+        url: '/',
+        templateUrl: '/templates/landing.html'
+      })
+      .state('album', {
+        url: "/album",
+        templateUrl: '/templates/album.html'
+      })
+      .state('collection', {
+        url: '/collection',
+        templateUrl: '/templates/collection.html'
+      });
+  }
+
+  angular
+  .module('rockJams', ['ui.router'])
+  .config(config);
+})();
